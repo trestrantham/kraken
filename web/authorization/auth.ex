9c707850ea4fc43f20f5ae0528b15211f76aafac
Tres Trantham <tres@trestrantham.com>
2016-01-09 01:09:46 -0800

    Add real login

--- web/authorization/auth.ex
+++ web/authorization/auth.ex
@@ -1,9 +1,19 @@
 defmodule Sync.Auth do
   alias Sync.User
+  alias Sync.Repo
+  import Ecto.Query, only: [from: 1, from: 2]
 
-  def validate_email_and_password(username, password) do
+  def validate_email_and_password(email, password) do
+    if user = Repo.one(from u in User, where: u.email == ^email) do
+      password_match = Comeonin.Bcrypt.checkpw(password, user.password_hash)
+    else
+      password_match = Comeonin.Bcrypt.dummy_checkpw
+    end
 
-    {:error, "reason"}
-    # {:ok, %User{}}
+    if password_match do
+      {:ok, user}
+    else
+      {:error, "email and password do not match"}
+    end
   end
 end
