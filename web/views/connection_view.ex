41c4e8bcf9290fa1f277b311f0c6c52699f9ead6
Tres Trantham <tres@trestrantham.com>
2016-01-24 21:52:42 -0800

    Tweak connections view

--- web/views/connection_view.ex
+++ web/views/connection_view.ex
@@ -1,3 +1,14 @@
 defmodule Kraken.ConnectionView do
   use Kraken.Web, :view
+
+  def fitbit_connection([]), do: nil
+  def fitbit_connection(nil), do: nil
+  def fitbit_connection(connections) do
+    connections |> Enum.find(fn(c) -> c.provider == "fitbit" end)
+  end
+
+  def expired?(%Kraken.DataConnection{} = connection) do
+    connection.expires_at && connection.expires_at < Guardian.Utils.timestamp
+  end
+  def expired?(_), do: true
 end
