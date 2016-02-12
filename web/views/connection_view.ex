b6c2409cfd6bdb1e2353f80e8225e83dd5f784e4
Tres Trantham <tres@trestrantham.com>
2016-02-11 20:06:29 -0800

    Update connection view

--- web/views/connection_view.ex
+++ web/views/connection_view.ex
@@ -1,10 +1,23 @@
 defmodule Kraken.ConnectionView do
   use Kraken.Web, :view
 
-  def fitbit_connection([]), do: nil
-  def fitbit_connection(nil), do: nil
-  def fitbit_connection(connections) do
-    connections |> Enum.find(fn(c) -> c.provider == "fitbit" end)
+  def connections_json(user_connections) do
+    ["fitbit", "runkeeper"]
+    |> Enum.map(fn(connection_type) ->
+      connection = connection_for_type(user_connections, connection_type) || %{state: false}
+
+      %{
+        name: connection_type,
+        state: connection.state || "available"
+      }
+    end)
+    |> Poison.encode!
+  end
+
+  def connection_for_type([], _), do: nil
+  def connection_for_type(connections, connection_type) do
+    connections
+    |> Enum.find(fn(c) -> c.provider == connection_type end)
   end
 
   def expired?(%Kraken.DataConnection{} = connection) do
