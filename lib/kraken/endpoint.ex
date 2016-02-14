7ed901bd10a0b98a9f30910daa0292f23c5a1bb5
Tres Trantham <tres@trestrantham.com>
2016-02-14 12:24:27 -0800

    Change channel endpoint to "/live"

--- lib/kraken/endpoint.ex
+++ lib/kraken/endpoint.ex
@@ -1,7 +1,7 @@
 defmodule Kraken.Endpoint do
   use Phoenix.Endpoint, otp_app: :kraken
 
-  socket "/socket", Kraken.UserSocket
+  socket "/live", Kraken.UserSocket
 
   # Serve at "/" the static files from "priv/static" directory.
   #
