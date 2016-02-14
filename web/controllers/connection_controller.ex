49cf8325e51e54e48e5c61e99e05f1a3d3fc9ec3
Tres Trantham <tres@trestrantham.com>
2016-02-14 12:27:49 -0800

    Passed munged data connections and providers to the template

--- web/controllers/connection_controller.ex
+++ web/controllers/connection_controller.ex
@@ -14,7 +14,7 @@ defmodule Kraken.ConnectionController do
   def index(conn, _params, current_user, _claims) do
     render conn, "index.html",
       current_user: current_user,
-      user_connections: connections(current_user)
+      connections: connections(current_user)
   end
 
   def request(conn, _params, current_user, _claims) do
@@ -45,10 +45,41 @@ defmodule Kraken.ConnectionController do
       connections: connections(current_user)
   end
 
-  defp connections(nil), do: []
+  defp connections(nil) do
+    Kraken.DataProvider.all
+    |> Enum.map(fn(provider) ->
+      state = provider[:state] || "available"
+      Map.merge(provider, %{state: state})
+    end)
+  end
+
   defp connections(%Kraken.User{} = user) do
-    user
+    user_connections = user
     |> Ecto.Model.assoc(:connections)
     |> Repo.all
+
+    Kraken.DataProvider.all
+    |> Enum.map(fn(provider) ->
+      lookup_connection(provider, user_connections)
+    end)
+  end
+
+  defp lookup_connection(provider, connections) do
+    state = provider.state || "available"
+
+    connection = connections
+    |> Enum.find(fn(c) ->
+      c.provider == String.downcase(provider)
+    end)
+
+    if connection do
+      if Kraken.User.expired?(connection) do
+        state = "expired"
+      else
+        state = "connected"
+      end
+    end
+
+    Map.merge(provider, %{state: state})
   end
 end
