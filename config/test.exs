4d59e55da419ef87fc94d0f0901026f18b03a39a
Tres Trantham <tres@trestrantham.com>
2016-02-16 00:15:53 -0800

    Shorten the new user test time

--- config/test.exs
+++ config/test.exs
@@ -15,3 +15,6 @@ config :kraken, Kraken.Repo,
   database: "kraken_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
+
+config :comeonin, :bcrypt_log_rounds, 4
+config :comeonin, :pbkdf2_rounds, 1
