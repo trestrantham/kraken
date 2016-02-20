0c61d940c00bcce7332ddfdb5dd11c3295c3df79
Tres Trantham <tres@trestrantham.com>
2016-02-20 11:49:18 -0800

    Style clean up

--- web/authorization/auth.ex
+++ web/authorization/auth.ex
@@ -4,15 +4,17 @@ defmodule Kraken.Auth do
   alias Kraken.{Repo,User}
 
   def validate_email_and_password(email, password) do
-    user = User
-    |> where(email: ^email)
-    |> Repo.one
+    user =
+      User
+      |> where(email: ^email)
+      |> Repo.one
 
-    password_match = if user do
-      Comeonin.Bcrypt.checkpw(password, user.password_hash)
-    else
-      Comeonin.Bcrypt.dummy_checkpw
-    end
+    password_match =
+      if user do
+        Comeonin.Bcrypt.checkpw(password, user.password_hash)
+      else
+        Comeonin.Bcrypt.dummy_checkpw
+      end
 
     if password_match do
       {:ok, user}
