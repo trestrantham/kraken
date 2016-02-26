71cb79977ddb583c22b7d0ba1cdd3ab26ff1bb33
Tres Trantham <tres@trestrantham.com>
2016-02-25 22:01:38 -0800

    Move connections migration

--- priv/repo/migrations/20160221094220_create_connections.exs
+++ priv/repo/migrations/20160221094220_create_connections.exs
@@ -3,7 +3,7 @@ defmodule Kraken.Repo.Migrations.CreateConnections do
 
   def change do
     create table(:connections) do
-      add :provider, :string
+      add :provider_id, references(:providers, on_delete: :delete_all)
       add :uid, :string
       add :user_id, references(:users, on_delete: :delete_all)
       add :token, :text
@@ -13,8 +13,8 @@ defmodule Kraken.Repo.Migrations.CreateConnections do
       timestamps
     end
 
-    create index(:connections, [:provider, :user_id], unique: true)
+    create index(:connections, [:provider_id, :user_id], unique: true)
     create index(:connections, [:expires_at])
-    create index(:connections, [:provider, :token])
+    create index(:connections, [:provider_id, :token])
   end
 end
