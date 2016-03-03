9fa33a7feb2d2757ad46a98b44fc953550c33aa7
Tres Trantham <tres@trestrantham.com>
2016-03-03 00:54:45 -0800

    Send provider ID with user status

--- web/models/provider.ex
+++ web/models/provider.ex
@@ -30,10 +30,10 @@ defmodule Kraken.Provider do
   def status_for_user(query, %User{} = user) do
     query
     |> join(:left, [p], c in Connection, p.id == c.provider_id and c.user_id == ^user.id)
-    |> select([p, c], %{name: p.name, message: p.message, state: fragment("CASE WHEN ? IS NULL THEN ? WHEN extract(epoch from now()) <= ? THEN 'connected' ELSE 'disconnected' END AS state", c.expires_at, p.state, c.expires_at)})
+    |> select([p, c], %{id: p.id, name: p.name, message: p.message, state: fragment("CASE WHEN ? IS NULL THEN ? WHEN extract(epoch from now()) <= ? THEN 'connected' ELSE 'disconnected' END AS state", c.expires_at, p.state, c.expires_at)})
   end
   def status_for_user(query, _) do
     query
-    |> select([p], %{name: p.name, message: p.message, state: p.state})
+    |> select([p], %{id: p.id, name: p.name, message: p.message, state: p.state})
   end
 end
