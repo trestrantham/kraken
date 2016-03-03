6d4dace52e0bb6cc7ec7c35f0f35aac728d0195a
Tres Trantham <tres@trestrantham.com>
2016-03-03 00:55:14 -0800

    Connect to the channel every page

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -4,4 +4,12 @@ let socket = new Socket("/live", {params: {token: window.userToken}})
 
 socket.connect()
 
-export default socket
+var channel = socket.channel(`users:${window.userToken}`);
+
+channel.join()
+  .receive("ok", () => {
+  })
+  .receive("error", () => {
+  })
+
+export default channel
